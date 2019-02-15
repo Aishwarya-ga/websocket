1f97fc7abcf6f47ed1e78d35f7e2b5fc63a973bb
aishwarya_g <aishu047@gmail.com>
2019-02-15 15:51:55 +0530

    modified search.component.ts

--- websocket/src/app/search/search.component.ts
+++ websocket/src/app/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import Stomp  from 'stompjs';
+import Stomp from 'stompjs';
 import SockJS from 'sockjs-client';
 import $ from 'jquery';
 
@@ -8,35 +8,41 @@ import $ from 'jquery';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent  {
+export class SearchComponent {
 
   public title = 'WebSockets chat';
   private stompClient;
 
   public search;
- 
-  constructor(){
+
+  constructor() {
     this.initializeWebSocketConnection();
   }
 
-  initializeWebSocketConnection(){
-    
+  initializeWebSocketConnection() {
+
+    /* this is the endpoint that we added in the registerStompEndpoints() 
+     * method in the server code.
+     */
     const socket = new SockJS('http://127.0.0.1:8080/socket');
     this.stompClient = Stomp.over(socket);
- 
+
     const _this = this;
-    this.stompClient.connect({}, function(frame) {
+    this.stompClient.connect({}, function (frame) {
       _this.stompClient.subscribe("/chat", (message) => {
-        if(message.body) {
-          $(".chat").append("<div class='message'>"+message.body+"</div>")
+        if (message.body) {
+          $(".chat").append("<div class='message'>" + message.body + "</div>")
           console.log(message.body);
         }
       });
     });
   }
 
-  sendMessage(search){
-    this.stompClient.send("/app/send/message" , {}, search);
+  sendMessage(search) {
+    /* (void) send(destination, headers = {}, body = '')
+     *     Send a message to a named destination.
+     */
+    this.stompClient.send("/app/send/message", {}, search);
     this.search = null;
   }
 }
